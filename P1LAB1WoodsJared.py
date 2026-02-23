c0cce9fa3159f23b9d6dc71a6b2d21fcda5d3ff0
Jared Woods <jaredw.15w@gmail.com>
2026-02-22 21:42:07 -0500

    Add files via upload

---
+++ P1LAB1WoodsJared.py
@@ -0,0 +1,9 @@
+# Jared Woods
+# 22FEB2026
+# P1LAB1
+#This project is to learn how to enter a input/output using Python in Visual Studio Code 
+
+
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+print(f"Welcome to CTI-110, {first_name} {last_name}!")
