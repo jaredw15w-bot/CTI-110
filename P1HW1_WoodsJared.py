bdab6d6f08ad505aa21bef6e6c3fc33fcc3556e6
Jared Woods <jaredw.15w@gmail.com>
2026-03-01 08:50:05 -0500

    Add files via upload

---
+++ P1HW1_WoodsJared.py
@@ -0,0 +1,29 @@
+# Jared Woods
+# 1MAR2026
+# P1HW1
+
+# -----Calculating Exponents-----
+
+print("-----Calculating Exponents-----\n")
+
+base_value = int(input("Enter an integer as the base value: "))
+exponent = int(input("Enter an integer as the exponent: "))
+
+result = base_value ** exponent
+
+print()
+print(f"{base_value} raised to the power of {exponent} is {result} !!\n")
+
+# -----Addition and Subtraction-----
+
+print("-----Addition and Subtraction-----\n")
+
+starting_integer = int(input("Enter a starting integer: "))
+add_integer = int(input("Enter an integer to add: "))
+subtract_integer = int(input("Enter an integer to subtract: "))
+
+final_result = starting_integer + add_integer - subtract_integer
+
+print()
+print(f"{starting_integer} + {add_integer} - {subtract_integer} is equal to {final_result}")
+
